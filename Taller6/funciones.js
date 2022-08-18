3c4e63d70ce255da029d1a72d95a951b82820e71
Ruben HQ <2992997670101@ingenieria.usac.edu.gt>
2022-08-17 19:44:48 -0600

    Taller 6
    Al presionar los botones, abre la imagen correspondiente

---
+++ Taller6/funciones.js
@@ -0,0 +1,13 @@
+function rosa_roja(){
+   let foto_rosaroja = "rosa_roja.jpg";
+   document.getElementById("imagen1").src= foto_rosaroja;
+  // document.getElementById("segundo_div").innerHTML=foto_rosaroja;
+
+   
+}
+
+function rosa_blanca(){
+    //console.log("Soy rosa blanca")
+    let foto_rosablanca = "rosa_blanca.jpg";
+    document.getElementById("imagen2").src= foto_rosablanca;
+}
